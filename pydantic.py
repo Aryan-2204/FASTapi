c9fa6ac8dc767a732b2be1a58dd9a28df840ef87
Aryan-2204 <akadam2204@gmail.com>
2026-05-22 09:08:06 +0530

    validationa

--- pydantic.py
+++ pydantic.py
@@ -1,14 +1,24 @@
-from pydantic import BaseModel
-from typing import List, Dict
+from pydantic import BaseModel, EmailStr, AnyUrl,Field ,field_validator
+from typing import List, Dict,Annotated
 
 class Patient(BaseModel):
-    name: str
+    name: Annotated[str, Field(min_length=1,title='Name')] #name is required and must be at least 1 character long 
+    email: EmailStr
+    linkdin: AnyUrl
     age: int
-    weight: float
-    married: bool 
-    allergies: List[str]
+    weight: Annotated[float, Field(gt=0, strict=True)] #weight must be greater than 0
+    married: bool #=false
+    allergies: List[str] #Optional[List[str]] = None
     contact_info: Dict[str, str]
     
+    @field_validator('email') #validator function that checks if the email is 2 modes before andd after
+    
+    @classmethod
+    def validate_email(cls, value):
+        if not value.endswith('@gmail.com'):
+            raise ValueError('Email must be a Gmail address')
+        return value
+     
 def insert_patient_data(patient: Patient):
        print(patient.name)
        print(patient.age)
@@ -19,7 +29,7 @@ def update_patient_data(patient: Patient):
        print(patient.age)
        print('updated')       
        
-patient_info = {'name': 'nitish', 'age': 30, 'weight': 70, 'married':True, 'allergies': ['nuts', 'fish'], 'contact_info':{'email':'nitish'}} #creating a dictionary with patient information, including name, age, and weight
+patient_info = {'name': 'nitish','email': 'nitish@gmail.com', 'linkdin':'https://www.linkedin.com/in/nitish', 'age': 30, 'weight': 70, 'married':True, 'allergies': ['nuts', 'fish'], 'contact_info':{'email':'nitish'}} #creating a dictionary with patient information, including name, age, and weight
 patient1 = Patient(**patient_info) #creating an instance of the Patient class using the patient_info dictionary
 
 insert_patient_data(patient1) #inserting patient data using the insert_patient_data function
